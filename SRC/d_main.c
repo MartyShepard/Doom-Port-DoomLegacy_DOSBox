d56edff032f795d4c9d622faf6fb155ce975d5e5
MartyShepard <Marty2GITHub@outlook.com>
2025-11-04 11:56:40 +0100

    Sync 1.44 r779
    - w66 save to SGI

--- SRC/d_main.c
+++ SRC/d_main.c
@@ -322,7 +322,7 @@
 
 // Version number: major.minor.revision
 const int  VERSION  = 144; // major*100 + minor
-const int  REVISION = 778; // for bugfix releases, should not affect compatibility. has nothing to do with svn revisions.
+const int  REVISION = 779; // for bugfix releases, should not affect compatibility. has nothing to do with svn revisions.
 const char VERSIONSTRING[] = "alpha2 (rev " SVN_REV ")";
 char VERSION_BANNER[80];
 
