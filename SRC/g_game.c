c9808d8e0dab838349dd2582bee3dad4e733c104
MartyShepard <Marty2GITHub@outlook.com>
2025-12-27 17:51:58 +0100

    Sync 1.47 r1400: w107 96 demoname
    [01400 ]: w107 96 demoname
    https://sourceforge.net/p/doomlegacy/svn/1400/tree/

--- SRC/g_game.c
+++ SRC/g_game.c
@@ -1,7 +1,7 @@
 // Emacs style mode select   -*- C++ -*- 
 //-----------------------------------------------------------------------------
 //
-// $Id: g_game.c 1399 2018-07-02 03:41:01Z wesleyjohnson $
+// $Id: g_game.c 1400 2018-07-02 03:41:30Z wesleyjohnson $
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
 // Copyright (C) 1998-2016 by DooM Legacy Team.
@@ -285,7 +285,7 @@ static   uint16_t  extra_dog_respawn = 0;  // save on extra tests
 #endif
 
 // Demo playback enables
-static const char * playdemo_name = NULL;
+static char * playdemo_name = NULL;  // malloc
 static byte  EN_demotic_109;  // old demo tic format
 static byte  EN_boom_longtics;  // 16 bit boom angle in demo tic
 
@@ -3401,7 +3401,14 @@ void G_DeferedPlayDemo (const char* name)
 //    COM_BufAddText(va("playdemo \"%s\"\n", name));
 
     // Using console command adds extra tics that cause sync problems.
-    playdemo_name = name;     // parameter
+    // Copy the demo name, as some are from stack buffers.
+    if( playdemo_name == NULL )
+    {
+        playdemo_name = (char*) malloc(MAX_WADPATH);
+    }
+    strncpy( playdemo_name, name, MAX_WADPATH-1);     // parameter
+    playdemo_name[MAX_WADPATH-1] = '\0';
+   
     gameaction = ga_playdemo;  // play demo after finishing this
 }
 
@@ -3979,16 +3986,8 @@ void G_DoneLevelLoad(void)
     demostarttime = I_GetTime ();
 }
 
-/*
-===================
-=
-= G_CheckDemoStatus
-=
-= Called after a death or level completion to allow demos to be cleaned up
-= Returns true if a new demo loop action will take place
-===================
-*/
 
+// Called after a death or level completion to allow demos to be cleaned up
 // reset engine variable set for the demos
 // called from stopdemo command, map command, and g_checkdemoStatus.
 void G_StopDemo(void)
@@ -4006,6 +4005,13 @@ void G_StopDemo(void)
     SV_StopServer();
 //    SV_StartServer();
     SV_ResetServer();
+   
+    // cleanup
+    if( playdemo_name )
+    {
+        free(playdemo_name);
+        playdemo_name = NULL;
+    }
 }
 
 // Called by G_DeferedInitNew, G_ReadDemoTiccmd, G_WriteDemoTiccmd
