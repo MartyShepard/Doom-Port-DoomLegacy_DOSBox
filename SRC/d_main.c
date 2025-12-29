b4db43ad9446f1e4ffc7388bb16d34344f465ab6
MartyShepard <Marty2GITHub@outlook.com>
2025-12-29 21:34:13 +0100

    Sync 1.47 r1421:
    [01421 ]: w108.0 v147.3 nr
    https://sourceforge.net/p/doomlegacy/svn/1421/tree/

--- SRC/d_main.c
+++ SRC/d_main.c
@@ -1,7 +1,7 @@
 // Emacs style mode select   -*- C++ -*-
 //-----------------------------------------------------------------------------
 //
-// $Id: d_main.c 1419 2019-01-29 08:01:42Z wesleyjohnson $
+// $Id: d_main.c 1421 2019-01-29 08:04:17Z wesleyjohnson $
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
 // Copyright (C) 1998-2016 by DooM Legacy Team.
@@ -315,13 +315,13 @@
 
 // Versioning
 #ifndef SVN_REV
-#define SVN_REV "1420"
+#define SVN_REV "1421"
 #endif
 
 
 // Version number: major.minor.revision
 const int  VERSION  = 147; // major*100 + minor
-const int  REVISION = 2;   // for bugfix releases, should not affect compatibility. has nothing to do with svn revisions.
+const int  REVISION = 3;   // for bugfix releases, should not affect compatibility. has nothing to do with svn revisions.
 static const char VERSIONSTRING[] = "(Rev " SVN_REV ")";
 //static const char VERSIONSTRING[] = "Beta (Rev " SVN_REV ")";
 char VERSION_BANNER[80];
