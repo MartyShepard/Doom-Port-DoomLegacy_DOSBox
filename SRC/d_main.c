04ee77ecd49d2936cd83172c1172feccd2cbd443
MartyShepard <Marty2GITHub@outlook.com>
2026-01-03 03:50:09 +0100

    Sync 1.48 r1493: w108_79 net textcmd
    [01493 ]: w108_79 net textcmd
    https://sourceforge.net/p/doomlegacy/svn/1493/tree/

--- SRC/d_main.c
+++ SRC/d_main.c
@@ -1,7 +1,7 @@
 // Emacs style mode select   -*- C++ -*-
 //-----------------------------------------------------------------------------
 //
-// $Id: d_main.c 1492 2019-12-19 07:51:00Z wesleyjohnson $
+// $Id: d_main.c 1493 2020-01-05 22:08:37Z wesleyjohnson $
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
 // Copyright (C) 1998-2016 by DooM Legacy Team.
@@ -316,7 +316,7 @@
 
 // Versioning
 #ifndef SVN_REV
-#define SVN_REV "1492"
+#define SVN_REV "1493"
 #endif
 
 
