8e1eebbe27572916a38e837fa7ec85f60c14c139
MartyShepard <Marty2GITHub@outlook.com>
2026-01-04 19:14:11 +0100

    Sync 1.48 r1526: w108_112 ver 1.48.3
    [01526 ]: w108_112 ver 1.48.3
    https://sourceforge.net/p/doomlegacy/svn/1526/tree/

--- SRC/d_main.c
+++ SRC/d_main.c
@@ -1,7 +1,7 @@
 // Emacs style mode select   -*- C++ -*-
 //-----------------------------------------------------------------------------
 //
-// $Id: d_main.c 1525 2020-05-09 12:07:29Z wesleyjohnson $
+// $Id: d_main.c 1526 2020-05-09 12:08:20Z wesleyjohnson $
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
 // Copyright (C) 1998-2016 by DooM Legacy Team.
@@ -316,13 +316,13 @@
 
 // Versioning
 #ifndef SVN_REV
-#define SVN_REV "1525"
+#define SVN_REV "1526"
 #endif
 
 
 // Version number: major.minor.revision
 const int  VERSION  = 148; // major*100 + minor
-const int  REVISION = 2;   // for bugfix releases, should not affect compatibility. has nothing to do with svn revisions.
+const int  REVISION = 3;   // for bugfix releases, should not affect compatibility. has nothing to do with svn revisions.
 static const char VERSIONSTRING[] = "(Rev " SVN_REV ")";
 //static const char VERSIONSTRING[] = "Beta (Rev " SVN_REV ")";
 char VERSION_BANNER[80];
